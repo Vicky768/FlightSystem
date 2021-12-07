926cd2ede3aea1b34e0dcc1c66e2e66e406f1d29
unknown <roysudeepto168@gmail.com>
2021-12-07 11:32:07 +0530

    first commit

---
+++ src/main/java/com/roy/flight_website/FlightSystem/FlightSystemApplication.java
@@ -0,0 +1,13 @@
+package com.roy.flight_website.FlightSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSystemApplication.class, args);
+	}
+
+}
